6d2688bfd066371cead7b7a7b5b1b08f941b551c
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-04-26 22:20:42 +0530

    commit-code added for exact winning position of 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,36 +1,38 @@
-public class SnakeLadder
+class SnakeLadder {
+public static void main(String[] args){
+System.out.println("Welcome to the Snake and Ladder Program");
+int PlayerPosition = 0;
+while(PlayerPosition < 100)
 {
-   public static void main(String[] args)
-   {
-   System.out.println("Welcome to the Snake & Ladder Program");
-   int PlayerPosition = 0;
-   while (PlayerPosition <= 100)
-   {
-   int RollDice = ((int)(Math.random() * 6)+1);
-   int Options = ((int)(Math.random() * 3)+1);
-   System.out.println("rolldice="+RollDice);
-   System.out.println("Options="+Options);
-   switch (Options)
-      {
-      case 1:
-         System.out.println("No Play");
-         break;
-      case 2:
-         System.out.println("climb the ladder");
-         PlayerPosition += RollDice;
-         System.out.println("move+"+RollDice);
-         break;
-      case 3:
-         System.out.println("Caught by snake");
-         PlayerPosition -= RollDice;
-         System.out.println("back-"+RollDice);
-         break;
-      }
-      if (PlayerPosition < 0)
-          PlayerPosition = 0;
-      }
-      if (PlayerPosition >= 100)
-      System.out.println("winner"+PlayerPosition);
-   }
+int options = ((int)(Math.random()*3)+1);
+int RollDice = ((int)(Math.random()*6)+1);
+System.out.println("Rolldice="+RollDice);
+switch (options){
+case 1:
+PlayerPosition = PlayerPosition;
+System.out.println("No_Play");
+System.out.println("Current Position =" +PlayerPosition);
+break;
+case 2:
+PlayerPosition +=RollDice;
+if(PlayerPosition>100){
+PlayerPosition-=RollDice;
+}
+System.out.println("Ladder");
+System.out.println("Current Position =" +PlayerPosition);
+break;
+case 3:
+PlayerPosition -=RollDice;
+if(PlayerPosition<0){
+PlayerPosition=0;
+}
+
+System.out.println("Snake");
+System.out.println("Current Position =" +PlayerPosition);
+break;
+default: System.out.println("Invalid");
+}
+ }
+}
 }
 
