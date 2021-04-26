7f0c1320124769e32e53f00ae0dae30dadf47a66
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-04-26 18:27:03 +0530

    commit-Added starting position of player

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,7 +1,8 @@
-class SnakeLadder
+public class SnakeLadder
 {
    public static void main(String[] args)
    {
    System.out.println("Welcome to the Snake & Ladder Program");
+   int PlayerPosition = 0;
    }
 }
