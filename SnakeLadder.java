5d1a55c9440425ca712311f43babc096ac14b2cf
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-04-26 19:23:57 +0530

    commit- added code for playing options

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,6 +5,24 @@ public class SnakeLadder
    System.out.println("Welcome to the Snake & Ladder Program");
    int PlayerPosition = 0;
    int RollDice = ((int)(Math.random() * 6)+1);
+   int Options = ((int)(Math.random() * 3)+1);
    System.out.println("rolldice="+RollDice);
+   System.out.println("Options="+Options);
+   switch (Options)
+      {
+      case 1:
+         System.out.println("No Play");
+         break;
+      case 2:
+         System.out.println("climb the ladder");
+         PlayerPosition += RollDice;
+         System.out.println("move+"+RollDice);
+         break;
+      case 3:
+         System.out.println("Caught by snake");
+         PlayerPosition -= RollDice;
+         System.out.println("back-"+RollDice);
+         break;
+      }
    }
 }
